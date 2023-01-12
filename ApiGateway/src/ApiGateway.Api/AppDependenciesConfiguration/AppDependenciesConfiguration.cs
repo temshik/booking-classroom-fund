63df55c85c5cc0aab73b9ab052edb884b71e3dcb
артём artem <hotko.artem@mail.ru>
2023-01-12 23:12:32 +0300

    Removed ApiGateway from PR

--- ApiGateway/src/ApiGateway.Api/AppDependenciesConfiguration/AppDependenciesConfiguration.cs
+++ ApiGateway/src/ApiGateway.Api/AppDependenciesConfiguration/AppDependenciesConfiguration.cs
@@ -1,5 +1,4 @@
-﻿using ApiGateway.Api.AppDependenciesConfiguration;
-using Ocelot.DependencyInjection;
+﻿using Ocelot.DependencyInjection;
 
 namespace ApiGateway.AppDependenciesConfiguration
 {
@@ -16,11 +15,10 @@ namespace ApiGateway.AppDependenciesConfiguration
         public static WebApplicationBuilder ConfigureDependencies(this WebApplicationBuilder builder)
         {
             var configuration = builder.Configuration;
-
-            builder.Services.AddConfigureJWT(builder);
-            builder.Services.AddOcelot(configuration);
-
             var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+
+            builder.Services.AddOcelot(configuration);
+            //configuration.AddJsonFile($"ocelot.{builder.Environment.EnvironmentName}.json", true, true);
             configuration.SetBasePath(builder.Environment.ContentRootPath);
             builder.Host.ConfigureAppConfiguration(config => config.AddJsonFile($"ocelot.{env}.json", true, true));
 
